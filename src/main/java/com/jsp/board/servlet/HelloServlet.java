2c8b3f69a08b1b71b0ef9330416b43c325473982
Inha Baek <tb1317@naver.com>
2023-09-09 23:15:23 +0900

    테스트중

--- src/main/java/com/jsp/board/servlet/HelloServlet.java
+++ src/main/java/com/jsp/board/servlet/HelloServlet.java
@@ -16,6 +16,6 @@ public class HelloServlet extends HttpServlet {
         req.setCharacterEncoding("UTF-8"); // 들어오는 데이터를 UTF-8로 해석하겠다.
         resp.setCharacterEncoding("UTF-8"); // 완성되는 HTML의 인코딩을 UTF-8로 하겠다.
         resp.setContentType("text/html; charset UTF-8"); // 브라우저에서 우리가 만든 결과물이 UTF-8이라는것을 알린다.
-        resp.getWriter().append("한글은깨질걸?!");
+        resp.getWriter().append("안녕하세요!");
     }
 }
